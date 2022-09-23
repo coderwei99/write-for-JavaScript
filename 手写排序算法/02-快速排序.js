b4580f23ae08804e6a523fe5ff91c05edd297716
coderwei <916606569@qq.com>
2022-09-24 02:56:15 +0800

    feat: 补全偏平化数组

--- 手写排序算法/02-快速排序.js
+++ 手写排序算法/02-快速排序.js
@@ -1,14 +1,63 @@
+let arr = [2, 1, 4, 6, 5, 4, 3, 2, 1];
 
-let arr = [2, 1, 4, 6, 5, 4, 3, 2, 1]
+// function quickSort(arr) {
+//   // 给个出口
+//   if (arr.length < 2) {
+//     return arr
+//   }
+//   let mid = arr[0]
+//   let minArr = arr.slice(1).filter(item => item <= mid) //比基准小的
+//   let maxArr = arr.slice(1).filter(item => item > mid)  //比基准大的
+//   return [...quickSort(minArr), mid, ...quickSort(maxArr)] //递归调用
+// }
+// console.log(quickSort(arr));
 
-function quickSort(arr) {
-  // 给个出口
-  if (arr.length < 2) {
-    return arr
-  }
-  let mid = arr[0]
-  let minArr = arr.slice(1).filter(item => item <= mid) //比基准小的
-  let maxArr = arr.slice(1).filter(item => item > mid)  //比基准大的
-  return [...quickSort(minArr), mid, ...quickSort(maxArr)] //递归调用
+
+var sortArray = function (nums) {
+  quickArray(nums, 0, nums.length - 1)
+  return nums
+};
+
+function quickArray(nums, start, end) {
+  if (start >= end) return
+  let mid = quick(nums, start, end)
+  quickArray(nums, start, mid - 1)
+  quickArray(nums, mid + 1, end)
 }
-console.log(quickSort(arr));
+
+function quick(nums, start, end) {
+  console.log(start, end);
+  let prvot = nums[start]
+  let left = start + 1
+  let right = end
+  while (left < right) {
+    while (left < right && nums[left] < prvot) {
+      left++
+    }
+    while (left < right && nums[right] > prvot) {
+      right--
+    }
+    /**    l   r   
+     * 4,3,1,7,5 ==> 4,3,1,7,5  ==> 1,3,4,7,5
+     * 1,2,3,4
+     * 
+     */
+    if (left < right) {
+      [nums[left], nums[right]] = [nums[right], nums[left]]
+      left++
+      right--
+    }
+  }
+
+  if (left === right && nums[right] > prvot) {
+    right--;
+  }
+  if (right !== start) {
+    [nums[start], nums[right]] = [nums[right], nums[start]]
+  }
+  return right
+}
+
+
+
+console.log(sortArray([15, 4, 2, 1]));
--- 面试常见的手写题/03-数组偏平化.js
+++ 面试常见的手写题/03-数组偏平化.js
@@ -1,4 +1,17 @@
-const _flat = (arr) => arr.reduce((prev, cur, index, arr) => {
-  return prev.concat(Array.isArray(cur) ? _flat(cur) : cur)
-}, [])
-console.log(_flat([1, 2, 3, [4, 5, 6, [2, 1, 2, [3, 2, 2, 2, 1, [22, 22, 2, 2, 2,]]]]]));
+// 数组扁平化
+/**
+ * 
+ * @param {*} arr: 原数组 
+ * @returns Array
+ */
+
+// 1. toString
+// const _flat = (arr) => arr.toString().split(',').map(item => Number(item))
+
+// 2. flat
+// console.log([[1, 2, 3], 4, 3, 2, [2, 1, 1,]].flat(Infinity));
+
+// 3. reduce
+const _flat = (arr) => arr.reduce((pre, cur) => pre.concat(Array.isArray(cur) ? _flat(cur) : cur), [])
+
+console.log(_flat([[1, 2, 3], 4, 3, 2, [2, 1, 1,]]))
